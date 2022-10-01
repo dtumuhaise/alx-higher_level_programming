24129f4524d1807b1dc16e9f051a4a409b015c2f
dtumuhaise <tumuhaise@gmail.com>
2022-10-01 04:17:31 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    maxkey = max(a_dictionary.values())
+
+    for key, value in a_dictionary.items():
+        if value is maxkey:
+            return key
