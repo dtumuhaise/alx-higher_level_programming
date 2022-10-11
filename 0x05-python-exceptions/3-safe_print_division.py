2b8be78905bec18b18f3fdb382b7d7baca38e4bf
dtumuhaise <tumuhaise@gmail.com>
2022-10-11 10:36:39 -0700

    int division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    div = 0
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        if b == 0:
+            print("Inside Result: None")
+        else:
+            print("Inside result: {}".format(div))
+    return div
