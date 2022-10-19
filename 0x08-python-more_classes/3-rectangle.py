edf40f35d5a3594b2af029c92d411ad28e61e31c
dtumuhaise <tumuhaise@gmail.com>
2022-10-19 00:18:05 -0700

    string rep

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
             return (2 * self.width) + (2 * self.height)
 
     def __repr__(self):
-        if type(self.width) == 0 or type(self.height) == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         else:
             return '\n'.join('#' * self.width for _ in range(self.height))
