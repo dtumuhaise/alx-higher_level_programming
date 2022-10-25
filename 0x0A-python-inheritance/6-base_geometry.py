21013e59c98c48a7da72faa4b5c7df3f6d5e040e
dtumuhaise <tumuhaise@gmail.com>
2022-10-25 01:40:58 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module defines a class
+"""
+
+
+class BaseGeometry:
+    """
+    define method area
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
