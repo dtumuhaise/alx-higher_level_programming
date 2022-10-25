c5316e925fb8cf14b9201f2b39a31843fb5a4e57
dtumuhaise <tumuhaise@gmail.com>
2022-10-24 23:59:04 -0700

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that returns the list if available
+attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    defining the function
+    """
+
+    return dir(obj)
