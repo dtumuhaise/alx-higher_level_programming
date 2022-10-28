1fb6a71e7cbdf0a7713d5e583ee22dc1068e59ca
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-28 10:40:10 +0300

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=""):
     read from filename
     """
 
-    with open(filename, "r", encoding="utf8") as f:
+    with open(filename, encoding="utf8") as f:
         for line in f:
             print(f.read())
