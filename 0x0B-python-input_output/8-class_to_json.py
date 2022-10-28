b1f1511f6c4b760396d06fc9fb196d45f7621753
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-28 21:11:03 +0300

    fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,13 +4,10 @@ module to define function
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """
     function returns dict with simple data structure for
     JSON serialization of an object
     """
 
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
