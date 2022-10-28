c7f30365363c033b9ff74b6cf21fc2c851a8bf72
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-28 15:43:56 +0300

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""
-module to add args to python list
-"""
-
-
+"""module to add args to python list"""
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
