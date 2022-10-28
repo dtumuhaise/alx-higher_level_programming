39d12011df0d03255d89580ee356e8aecc4f0f9f
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-28 10:14:17 +0300

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function to read text
+"""
+
+
+def read_file(filename=""):
+    """
+    read from filename
+    """
+
+    with open("filename", "r", encoding="utf-8") as f:
+        print(f.read())
