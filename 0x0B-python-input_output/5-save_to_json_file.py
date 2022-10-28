21458fbf46a91020755a08ccb27c38464fdea334
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-28 12:56:57 +0300

    fix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,5 +12,5 @@ def save_to_json_file(my_obj, filename):
     define function that writes object to test file
     """
 
-    with open(filename) as my_obj:
+    with open(filename, "w") as my_obj:
         return json.load(my_obj)
