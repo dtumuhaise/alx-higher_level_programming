6b9eed94389f5e9d91576f632e9c1f3b6c5133b4
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-28 12:50:06 +0300

    save object to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module to define a function
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    define function that writes object to test file
+    """
+
+    with open(filename) as my_obj:
+        return json.load(my_obj)
