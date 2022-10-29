d9bfe22c4d7838a40b6e8016a27a936e0cbd5e13
Davis Tumuhaise <tumuhaise@gmail.com>
2022-10-29 23:01:57 +0300

    update triangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,7 +85,31 @@ class Rectangle(Base):
         to stdout
         """
 
-        for i in range(self.__height):
-            for j in range(self.__width):
-                print('#', end='')
-            print()
+        rectangle = ""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+        print(rectangle, end="")
+
+    def __str__(self):
+        """override the str method
+        """
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
+
+    def update(self, *args):
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
+
+            
+
+        
+
