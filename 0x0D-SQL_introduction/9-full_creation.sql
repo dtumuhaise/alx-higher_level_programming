1f0a525c28b456a89701eac1229380076dc88c79
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-01 11:58:22 +0300

    update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table IF NOT EXISTS
+INSERT INTO IF NOT EXISTS second_table 
     (id, name, score) 
 VALUES (1, "JOHN", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8);
