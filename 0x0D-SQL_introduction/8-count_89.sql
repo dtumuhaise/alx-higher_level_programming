45dce12e66e4171636d38fac5c6682e186b2fb00
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-01 11:53:54 +0300

    update multiple

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- script displays number of records with id = 89
+SELECT id FROM first_table WHERE id = 89;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- creates second table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table 
+    (id, name, score) 
+VALUES (1, "JOHN", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8);
