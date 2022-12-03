0d1bf2af3394fa4ad7f77fd134d1981fa85298c1
dtumuhaise <tumuhaise@gmail.com>
2022-12-04 01:45:49 +0300

    update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    PRIMARY KEY(id),
+CREATE TABLE IF NOT EXISTS states 
+(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL);
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
