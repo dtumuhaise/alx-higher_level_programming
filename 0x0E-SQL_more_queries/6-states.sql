7408b5649d5115fbab80662a7d870cb0a542148b
dtumuhaise <tumuhaise@gmail.com>
2022-12-04 01:29:52 +0300

    states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE NOT NULL GENERATED ALWAYS,
+    name VARCHAR(256) NOT NULL);
