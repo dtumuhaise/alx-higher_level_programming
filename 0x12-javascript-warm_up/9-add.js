1293bd9c31e7c09f2b2b63b05dd70257190d661a
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-23 14:41:56 +0300

    9. Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+
+const args = process.argv.slice(2);
+if (args.length > 2) {
+  console.log(NaN);
+} else {
+  console.log(add(parseInt(args[0]), parseInt(args[1])));
+}
