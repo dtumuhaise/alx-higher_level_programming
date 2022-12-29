8b9cd042e02073d4dc293302cd6e3716249364ca
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-29 16:20:18 +0300

    4. Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // prints rectangle with character X
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  // exchange width and height of the rectangle
+  rotate () {
+    let temp;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  // double() multiplies the width and height of rect by 2
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
