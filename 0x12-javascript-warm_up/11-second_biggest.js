ba076637756bcf75997c09fdcae654c78e740b12
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-29 10:57:27 +0300

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-if (!args[0]) {
-  console.log(0);
-} else if (args.length === 1) {
+if (process.argv.length < 4) {
   console.log(0);
 } else {
-  console.log(Math.max(parseInt(args)));
+  const size = process.argv.length;
+  const arr = [];
+  for (let i = 2; i < size; i++) {
+    arr[i - 2] = parseInt(process.argv[i]);
+  }
+  arr.sort(function (a, b) { return b - a; });
+  console.log(arr[1]);
 }
