29bef96e1e3b1a34d6831ea775279648af446738
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-29 20:47:33 +0300

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ module.exports = class Square extends Rectangle {
     if (c) {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.height; j++) {
-          process.stdout.write('C');
+          process.stdout.write(c);
         }
         console.log('');
       }
