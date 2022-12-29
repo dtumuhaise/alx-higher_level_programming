948e6c394e0a320b2780ec96225b8a5c0f3d757e
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-29 15:28:23 +0300

    2. Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    this.width  = w;
+    this.height = h;
+	}
+}
+
+if (this.w <= 0 || this.h <= 0) {
+	const rectangle1 = new Rectangle();
+}
+
+module.exports = Rectangle
