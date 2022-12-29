5624d7d0a42b9620b879f865fc70f310a46ea341
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-29 17:14:16 +0300

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  // prints rectangle using character 'C'
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.height; j++) {
+          process.stdout.write('C');
+        }
+        console.log('');
+      }
+    }
+  }
+};
