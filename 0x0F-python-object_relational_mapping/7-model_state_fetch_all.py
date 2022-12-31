bf4e91d1df607e176927127be3b03148c7bd1185
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-31 20:27:11 +0300

    fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,23 +4,28 @@ class definition of a state and intance Base
 """
 
 
+from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+
+    """ create an engine """
     engine = create_engine(
-        "mysql+pymysql://{}:{}@localhost/{}".format(
+        "mysql+pymysql://{}:{}@localhost:3306/{}".format(
             argv[1], argv[2], argv[3]
         )
     )
 
-    Base.metadata.create_all(engine)
+    """create configured session class"""
     Session = sessionmaker(bind=engine)
+
+    """create a session"""
     session = Session()
+    Base.metadata.create_all(engine)
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
     session.close()
