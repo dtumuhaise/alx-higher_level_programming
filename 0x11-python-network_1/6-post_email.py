9635ff694ad21d3f7833a44fe49d478d6bbc8b18
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-20 02:32:34 +0300

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,11 +6,12 @@ send POST request to passed URL
 import sys
 import requests
 
+if __name__ == "__main__":
 
-url = sys.argv[1]
-email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = {"email": email}
-response = requests.post(url, data=data)
+    data = {"email": email}
+    response = requests.post(url, data=data)
 
-print(response.text)
+    print(response.text)
