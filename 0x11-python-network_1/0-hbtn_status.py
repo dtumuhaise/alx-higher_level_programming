bfc4f14620b57794ad694f1eb1441635206d56fd
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-20 01:16:00 +0300

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
  script that fetches https://alx-intranet.hbtn.io/status
  """
 
-import urllib.request
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import urllib.request
+
     url = 'https://alx-intranet.hbtn.io/status'
-
     with urllib.request.urlopen(url) as response:
         data = response.read()
         print("Body response:")
