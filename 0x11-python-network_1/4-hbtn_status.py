d7ce6791931dcb6bb295dc7a54e63528ed457b39
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-20 03:38:02 +0300

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.content))
+    print("\t- content: {}".format(response.text))
